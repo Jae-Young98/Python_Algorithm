75a38dcbc66bbfe04fc85d76ba2c2beecbefba1f
Jae-Young98 <86467141+Jae-Young98@users.noreply.github.com>
2022-09-14 11:51:43 +0900

    ex 03

--- hw1.py
+++ hw1.py
@@ -1,4 +1,5 @@
 # ex 1
+profiles = []
 
 def bmi(N):
     list = []
@@ -8,6 +9,7 @@ def bmi(N):
         list.append(userInfo)
         list[i].append(calc_bmi(list[i][1], list[i][2]))
         list[i].append(Judge(list[i][3]))
+        profiles.append(list[i])
     return list
 
 
@@ -29,4 +31,25 @@ def Judge(bmi):
         return '비만'
 
 
+# ex 2
+def bmi_list_min(profiles):
+    min_bmi = profiles[0][3]
+    min_idx = 0
+    for i in range(len(profiles)):
+        if min_bmi > profiles[i][3]:
+            min_bmi = profiles[i][3]
+            min_idx = i
+    return profiles[min_idx]
+
+
+# ex 3
+def bmi_dict(profiles):
+    userDict = {}
+    for i in range(len(profiles)):
+        userDict[profiles[i][0]] = profiles[i][1:]
+    return userDict
+
 print(bmi(3))
+print(profiles)
+print(bmi_list_min(profiles))
+print(bmi_dict(profiles))
